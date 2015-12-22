10d260adb64b3348eebfac73f0361665b08f2c51
Matthias Leitner <matthias.leitner@runtastic.com>
2015-12-22 11:14:28 +0100

    setup index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint node: true */
 'use strict';
 
+require('shelljs/global');
+var info = require('git-repo-info')();
+
+revisionCommen = function() {
+	return '<!--\n' + info.sha + '\n-->';
+}
+
 module.exports = {
-  name: 'ember-cli-revision-comment'
+  name: 'ember-cli-revision-comment',
+
+  contentFor: function(type, config) {
+    if(config.environment === 'production' || type !== 'body-footer') {
+      return;
+    }
+
+    return revisionCommen();
+  }
 };
